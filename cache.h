3f6a87f28bb99f60f5088f81a55ba64194427c23
Hugh-ONeill <grumpyrumpus@gmail.com>
2017-10-30 19:19:01 -0400

    Starting Cache

--- cache.h
+++ cache.h
@@ -1,64 +1,83 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-struct cache_blk_t { /* note that no actual data will be stored in the cache */
-  unsigned long tag;
-  char valid;
-  char dirty;
-  unsigned LRU;	/*to be used to build the LRU stack for the blocks in a cache set*/
-};
-
-struct cache_t {
-	// The cache is represented by a 2-D array of blocks. 
-	// The first dimension of the 2D array is "nsets" which is the number of sets (entries)
-	// The second dimension is "assoc", which is the number of blocks in each set.
-  int nsets;					// number of sets
-  int blocksize;				// block size
-  int assoc;					// associativity
-  int mem_latency;				// the miss penalty
-  struct cache_blk_t **blocks;	// a pointer to the array of cache blocks
-};
-
-struct cache_t *
-	cache_create(int size, int blocksize, int assoc, int mem_latency)
+struct cache_blk_t
 {
-  int i;
-  int nblocks = 1;			// number of blocks in the cache
-  int nsets = 1;			// number of sets (entries) in the cache
+    unsigned long tag;
+    char valid;
+    char dirty;
+    unsigned LRU;
+};
 
-  // YOUR JOB: calculate the number of sets and blocks in the cache
-  //
-  // nblocks = X;
-  // nsets = Y;
+struct cache_t
+{
+    // The cache is represented by a 2-D array of blocks.
+    // The first dimension of the 2D array is "nsets" which is the number of sets (entries)
+    // The second dimension is "assoc", which is the number of blocks in each set.
+    int nsets;                      // number of sets
+    int blocksize;                  // block size
+    int assoc;                      // associativity
+    int mem_latency;                // the miss penalty
+    struct cache_blk_t **blocks;    // a pointer to the array of cache blocks
+};
 
-  struct cache_t *C = (struct cache_t *)calloc(1, sizeof(struct cache_t));
-		
-  C->nsets = nsets; 
-  C->assoc = assoc;
-  C->mem_latency = mem_latency;
+struct cache_t * cache_create(int size, int blocksize, int assoc, int mem_latency)
+{
+    int i;
+    int nblocks = (size * 1000) / blocksize;    // number of blocks in the cache
+    int nsets = nblocks / assoc;                // number of sets (entries) in the cache
+    struct cache_t *C = (struct cache_t *)calloc(1, sizeof(struct cache_t));
 
-  C->blocks= (struct cache_blk_t **)calloc(nsets, sizeof(struct cache_blk_t *));
+    C->nsets = nsets;
+    C->blocksize = blocksize;
+    C->assoc = assoc;
+    C->mem_latency = mem_latency;
 
-  for(i = 0; i < nsets; i++) {
+    C->blocks = (struct cache_blk_t **)calloc(nsets, sizeof(struct cache_blk_t *));
+
+    for(i = 0; i < nsets; i++)
+    {
 		C->blocks[i] = (struct cache_blk_t *)calloc(assoc, sizeof(struct cache_blk_t));
-	}
+    }
 
-  return C;
+    return C;
 }
 
 int cache_access(struct cache_t *cp, unsigned long address, int access_type)
 {
-  //
-  // Based on "address", determine the set to access in cp and examine the blocks
-  // in the set to check hit/miss and update the golbal hit/miss statistics
-  // If a miss, determine the victim in the set to replace (LRU). 
-  //
-  // The function should return the hit_latency, which is 0, in case of a hit.
-  // In case of a miss, the function should return mem_latency if no write back is needed.
-  // If a write back is needed, the function should return 2*mem_latency.
-  // access_type (0 for a read and 1 for a write) should be used to set/update the dirty bit.
-  // The LRU field of the blocks in the set accessed should also be updated.
+    //
+    // Based on "address", determine the set to access in cp and examine the blocks
+    // in the set to check hit/miss and update the golbal hit/miss statistics
+    // If a miss, determine the victim in the set to replace (LRU).
+    //
+    // The function should return the hit_latency, which is 0, in case of a hit.
+    // In case of a miss, the function should return mem_latency if no write back is needed.
+    // If a write back is needed, the function should return 2*mem_latency.
+    // access_type (0 for a read and 1 for a write) should be used to set/update the dirty bit.
+    // The LRU field of the blocks in the set accessed should also be updated.
 
-	return(cp->mem_latency);
+    int penalty = cp->mem_latency;
+	
+	struct cache_blk_t block;
+	int offset = address % (cp->blocksize);
+	int block_address = address / (cp->blocksize);
+	int set_index = block_address % (cp->nsets);
+	
+	int offset_size = log2(cp->blocksize);
+	int index_size = log2(cp->nsets);
+	int shift_value = offset_size + index_size;
+	
+	int tag = address >> shift_value;
+	
+	int i;
+	struct cache_blk_t * set = cp->blocks[set_index];
+	for(i = 0; i < (cp->assoc); i++)
+	{
+		block = set[i]
+		// Process Block
+	}
+
+    return(penalty);
 }
