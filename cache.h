ca58178304140a97acd8ab2f2df8b9c799abed21
Hugh-ONeill <grumpyrumpus@gmail.com>
2017-11-01 18:00:41 -0400

    tag tester

--- cache.h
+++ cache.h
@@ -1,69 +1,143 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-struct cache_blk_t { /* note that no actual data will be stored in the cache */
-  unsigned long tag;
-  char valid;
-  char dirty;
-  unsigned LRU;	/*to be used to build the LRU stack for the blocks in a cache set*/
-};
-
-struct cache_t {
-	// The cache is represented by a 2-D array of blocks. 
-	// The first dimension of the 2D array is "nsets" which is the number of sets (entries)
-	// The second dimension is "assoc", which is the number of blocks in each set.
-  int nsets;					// number of sets
-  int blocksize;				// block size
-  int assoc;					// associativity
-  int mem_latency;				// the miss penalty
-  struct cache_blk_t **blocks;	// a pointer to the array of cache blocks
-};
-
-struct cache_t *
-	cache_create(int size, int blocksize, int assoc, int mem_latency)
+struct cache_blk_t
 {
-  int i;
-  int nblocks = (size*1000)/blocksize;	// number of blocks in the cache
-  int nsets = nblocks/assoc;	// number of sets (entries) in the cache 
+    unsigned long tag;
+    char valid;
+    char dirty;
+    unsigned LRU;
+};
 
-  struct cache_t *C = (struct cache_t *)calloc(1, sizeof(struct cache_t));
-		
-  C->nsets = nsets; 
-  C->assoc = assoc;
-  C->mem_latency = mem_latency;
+struct cache_t
+{
+    // The cache is represented by a 2-D array of blocks.
+    // The first dimension of the 2D array is "nsets" which is the number of sets (entries)
+    // The second dimension is "assoc", which is the number of blocks in each set.
+    int nsets;                      // number of sets
+    int blocksize;                  // block size
+    int assoc;                      // associativity
+    int mem_latency;                // the miss penalty
+    struct cache_blk_t **blocks;    // a pointer to the array of cache blocks
+};
 
-  C->blocks= (struct cache_blk_t **)calloc(nsets, sizeof(struct cache_blk_t *));
+struct cache_t * cache_create(int size, int blocksize, int assoc, int mem_latency)
+{
+    int i;
+    int nblocks = (size * 1024) / blocksize;    // number of blocks in the cache
+    int nsets = nblocks / assoc;                // number of sets (entries) in the cache
+    struct cache_t *C = (struct cache_t *)calloc(1, sizeof(struct cache_t));
 
-  for(i = 0; i < nsets; i++) {
+    C->nsets = nsets;
+    C->blocksize = blocksize;
+    C->assoc = assoc;
+    C->mem_latency = mem_latency;
+
+    C->blocks = (struct cache_blk_t **)calloc(nsets, sizeof(struct cache_blk_t *));
+
+    for(i = 0; i < nsets; i++)
+    {
 		C->blocks[i] = (struct cache_blk_t *)calloc(assoc, sizeof(struct cache_blk_t));
-	}
+    }
 
-  return C;
+    return C;
 }
 
 int cache_access(struct cache_t *cp, unsigned long address, int access_type)
 {
+    //
+    // Based on "address", determine the set to access in cp and examine the blocks
+    // in the set to check hit/miss and update the golbal hit/miss statistics
+    // If a miss, determine the victim in the set to replace (LRU).
+    //
+    // The function should return the hit_latency, which is 0, in case of a hit.
+    // In case of a miss, the function should return mem_latency if no write back is needed.
+    // If a write back is needed, the function should return 2*mem_latency.
+    // access_type (0 for a read and 1 for a write) should be used to set/update the dirty bit.
+    // The LRU field of the blocks in the set accessed should also be updated.
+	
+    int penalty = cp->mem_latency;
+	
+	// Setting Sizes and Computing Values
+	unsigned int offset_size = log2(cp->blocksize);
+	unsigned int index_size = log2(cp->nsets);
+	unsigned int tag_size = 32 - (offset_size + index_size);
+	
+	unsigned int offset_lshift = tag_size + index_size;
+	unsigned int offset_rshift = 32 - offset_size;
+	unsigned int index_lshift = tag_size;
+	unsigned int index_rshift = 32 - index_size;
+	unsigned int tag_rshift = offset_size + index_size;
+	
+	unsigned int offset = address << offset_lshift;
+	offset = offset >> offset_rshift;
+	unsigned int index = address << index_lshift;
+	index = index >> index_rshift;
+	unsigned int tag = address >> tag_rshift;
+	
+	// Check Blocks
+	int i;
+	int set_size = cp->assoc;
+	int hit = 0;
+	struct cache_blk_t * block;
+	struct cache_blk_t * LRU_block;
+	struct cache_blk_t * set = cp->blocks[index];
+	
+	int LRU[set_size];
+	int LRU_val = 0;
+	
+	for(i = 0; i < set_size; i++)
+	{
+		block = set[i];
+		
+		if (block->valid == 1)
+		{
+			if (block->tag == tag)
+			{	// HIT
+				hit = 1;
+				break;
+			}
+		}
+		else
+		{	// MISS
+			block->LRU = 1;
+		}
+	}
+	
+	if (hit == 1):
+	{	// HIT
+		// LRU Update
+		penalty = 0;
+	}
+	else
+	{	// MISS
+		// LRU Get
+		// block = LRU;
+		if (block->dirty == 1)
+		{	// Write Back
+			penalty = penalty * 2;
+		}
+		else
+		{	// No Write Back
+			// penalty = penalty;
+			0;
+		}
+		
+		if (access_type == 0)
+		{	// READ
+			// Read Resets Dirty Bit On Miss
+			block->dirty = 0;
+			block->valid = 1;
+		}
+	}
 
-  int byteAddress = address/cp->blocksize;
-  int set = (byteAddress) % cp->nsets;
-  int hit = 0;
-  
-  for(int i = 0; i < cp->assoc; i++){
-  	if(cp[set][i] == byteAddress{//mask bits?
-  		hit = 1;
-  	}
-  }
-  
-  //
-  // Based on "address", determine the set to access in cp and examine the blocks
-  // in the set to check hit/miss and update the golbal hit/miss statistics
-  // If a miss, determine the victim in the set to replace (LRU). 
-  //
-  // The function should return the hit_latency, which is 0, in case of a hit.
-  // In case of a miss, the function should return mem_latency if no write back is needed.
-  // If a write back is needed, the function should return 2*mem_latency.
-  // access_type (0 for a read and 1 for a write) should be used to set/update the dirty bit.
-  // The LRU field of the blocks in the set accessed should also be updated.
+	if (access_type == 1)
+	{	// WRITE
+		// Write Always Sets Dirty Bit
+		block->dirty = 1;
+		block->valid = 1;
+	}
 
-	return(cp->mem_latency);
+    return(penalty);
 }
--- tag.c
+++ tag.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
-#include <math.h>
+#include <cache.h>
 
 // gcc tag.c -o tag -lm
 
 int main(int argc, const char* argv[])
 {
-	unsigned int blocksize = atoi(argv[1]);
-	unsigned int nsets = atoi(argv[2]);
-	unsigned long address = atoi(argv[3]);
+	unsigned int size = 16;
+	unsigned int assoc = 4;
+	unsigned int bsize = 8;
+	unsigned int mem_latency = 20;
 	
-	unsigned int offset_size = log2(blocksize);
-	unsigned int index_size = log2(nsets);
-	unsigned int tag_size = 32 - (offset_size + index_size);
+	unsigned int address = 12345678;
+	// 00000000 10111100 01100001 01001110
 	
-	unsigned int offset_lshift = tag_size + index_size;
-	unsigned int offset_rshift = 32 - offset_size;
-	unsigned int index_lshift = tag_size;
-	unsigned int index_rshift = 32 - index_size;
-	unsigned int tag_rshift = offset_size + index_size;
-
-	unsigned int offset = address << offset_lshift;
-	offset = offset >> offset_rshift;
-	unsigned int index = address << index_lshift;
-	index = index >> index_rshift;
-	unsigned int tag = address >> tag_rshift;
+	struct cache_t *cache;
+	cache = cache_create(size, bsize, assoc, mem_latency);
 	
-	printf("blocksize: %u\nsets: %u\naddress: %u\ntag: %u\nindex: %u\n offset: %u", blocksize, nsets, address, tag, index, offset);
-	
-	return(0);
+	cycle_number = cache_access(cache, address, 1);
+	printf("read: %u", cycle_number);
+	cycle_number = cache_access(cache, address, 0);
+	printf("write: %u", cycle_number);
 }
